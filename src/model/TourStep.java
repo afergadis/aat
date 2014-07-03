c76daa86d44547fa6a5966d870eb36b9b84010e6
afergadis <fergadis.aris@gmail.com>
2014-07-03 20:19:35 +0300

    Καταχωρεί μία πρόταση επόμενου εκθέματος και όχι λίστα προτεινόμενων.

--- src/model/TourStep.java
+++ src/model/TourStep.java
@@ -1,70 +1,43 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
 
 /**
- * Κρατάει τα στοιχεία ενός αντικειμένου και εξάγει δεδομένα σχετικά με τους
- * προτεινόμενους συνδέσμους για επιπλέον πληροφορίες και για τις προτάσεις για
- * την επόμενη επίσκεψη του χρήστη.
+ * Κρατάει τα στοιχεία ενός εκθέματος και δεδομένα σχετικά με την περιγραφή και 
+ * την πρόταση για την επόμενη επίσκεψη του χρήστη.
  * 
  */
 public class TourStep {
 	private final boolean start;
 	private final String entity;
 	private final String descr;
-	private final List<String> links;
-	private final List<String> suggestions;
+	private final String suggestion;
 
 	/**
-	 * Για ένα αντικείμενο της αρχαίας αγοράς κρατάει πληροφορίες σχετικές με
-	 * τους προτεινόμενους συνδέσμους και τα προτεινόμενα επόμενα αντικείμενα.
+	 * Κρατάει τα στοιχεία ενός εκθέματος και δεδομένα σχετικά με την περιγραφή και 
+	 * την πρόταση για την επόμενη επίσκεψη του χρήστη.
 	 * 
 	 * @param start
-	 *            δηλώνει ότι είναι το εναρκτήριο αντικείμενο
+	 *            δηλώνει ότι είναι το εναρκτήριο έκθεμα
 	 * @param name
 	 *            το όνομα του αντικειμένου
 	 * @param descr
 	 *            περιγραφή
-	 * @param s1
-	 *            πρώτη πρόταση για επόμενο αντικείμενο
-	 * @param s2
-	 *            δεύτερη πρόταση για επόμενο αντικείμενο
-	 * @param l1
-	 *            πρώτος σύνδεσμος για επιπλέον πληροφορίες
-	 * @param l2
-	 *            δεύτερος σύνδεσμος για επιπλέον πληροφορίες
+	 * @param next
+	 *            πρόταση για επόμενο έκθεμα
 	 */
-	public TourStep(boolean start, String name, String descr, String s1,
-			String s2, String l1, String l2) {
+	public TourStep(boolean start, String name, String descr, String next) {
 		this.start = start;
 		this.entity = name;
 		this.descr = descr;
-		links = new ArrayList<>();
-		suggestions = new ArrayList<>();
-
-		suggestions.add(s1);
-		suggestions.add(s2);
-		links.add(l1);
-		links.add(l2);
+		suggestion = next;
 	}
 
 	/**
-	 * Δίνει σύνδεσμους σχετικούς με το αντικείμενο.
 	 * 
-	 * @return λίστα με τα ονόματα που αντιστοιχούν σε αντικείμενα της δομής
+	 * @return επιστρέφει το επόμενο προτεινόμενο έκθεμα
 	 */
-	public List<String> getLinks() {
-		return links;
-	}
-
-	/**
-	 * Δίνει σύνδεσμους προτάσεις για το επόμενο έκθεμα.
-	 * 
-	 * @return λίστα με τα ονόματα που αντιστοιχούν σε αντικείμενα της δομής
-	 */
-	public List<String> getSuggestions() {
-		return suggestions;
+	public String getSuggestion() {
+		return suggestion;
 	}
 
 	/**
